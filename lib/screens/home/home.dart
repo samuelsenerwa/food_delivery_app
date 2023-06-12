7bef4c71bf1a01e90f74523fd1a297df3f76a47d
senerwa <samuelsenerwa>
2023-06-12 11:40:44 +0300

    left-icon & searchbar

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,27 +3,24 @@ import 'package:food_delivery_app/constants/colors.dart';
 import 'package:food_delivery_app/widget/custom_app_bar.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
 //first icon to be selected
-var selected = 0;
+  var selected = 0;
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        backgroundColor: kBackground,
-        body: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-                CustomAppBar(),
-            ],
-        ),
+    return const Scaffold(
+      backgroundColor: kBackground,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(Icons.arrow_back_ios_outlined, Icons.search_outlined),
+        ],
+      ),
     );
   }
 }
--- lib/widget/custom_app_bar.dart
+++ lib/widget/custom_app_bar.dart
@@ -1,41 +1,37 @@
 import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget {
-  // const CustomAppBar({super.key});
+  const CustomAppBar(this.leftIcon, this.rightIcon, {this.leftCallback});
 
-final IconData leftIcon;
-final IconData rightIcon;
-final Function? leftCallback;
-
-CustomAppBar(
-  this.leftIcon,
-  this.rightIcon,
-  {
-    this.leftCallback,
-  }
-);
+  final IconData leftIcon;
+  final IconData rightIcon;
+  final Function? leftCallback;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(
         top: MediaQuery.of(context).padding.top,
-        left: 25,
+        left: 25.0,
         right: 25,
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-            padding: EdgeInsets.fromLTRB(16, 8, 8, 8),
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.white,
+          GestureDetector(
+            onTap: leftCallback != null ? () => leftCallback!() : null,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+              child: Icon(leftIcon),
             ),
           ),
           Container(
-            padding: EdgeInsets.all(8),
-            decoration: BoxDecoration(
+            padding: const EdgeInsets.all(8),
+            decoration: const BoxDecoration(
               shape: BoxShape.circle,
               color: Colors.white,
             ),
@@ -43,6 +39,6 @@ CustomAppBar(
           )
         ],
       ),
-    ); 
+    );
   }
 }
